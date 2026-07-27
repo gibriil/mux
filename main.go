6bd4b2523a5012cb8a477df3fdaf9a2477e660f2
Peter Beard <musicmandanceman@yahoo.com>
2026-07-26 21:59:53 -0600

    Basic multiplexer in place

--- main.go
+++ main.go
@@ -11,5 +11,44 @@ type Multiplexer struct {
 	ErrorLog *log.Logger
 }
 
-func (m *Multiplexer) Process() error
-func (m *Multiplexer) ProcessWithContext(ctx context.Context) error
+func NewMux() *Mux {
+	return &Mux{
+		routes: map[Route]Handler{},
+	}
+}
+
+type Source interface {
+	Next(context.Context) (Routable, error)
+}
+
+func (m *Multiplexer) Process() error {
+	return m.ProcessWithContext(context.Background())
+}
+
+func (m *Multiplexer) ProcessWithContext(ctx context.Context) error {
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+		}
+
+		route, err := m.Source.Next(ctx)
+		if err != nil {
+			return err
+		}
+
+		if err := m.Handler.Process(route); err != nil {
+			m.logError(err)
+			continue
+		}
+	}
+}
+
+func (m *Multiplexer) logError(err error) {
+	if m.ErrorLog != nil {
+		m.ErrorLog.Println(err)
+	} else {
+		log.Printf("Route Handler produced error: %v", err)
+	}
+}
--- mux.go
+++ mux.go
@@ -1,15 +1,9 @@
 package mux
 
-type mux struct {
-	routes map[string]Handler
-}
-
-func (m *mux) Handle(route string, handler Handler)
-func (m *mux) HandleFunc(route string, handler HandlerFunc)
-func (m *mux) Process(r Routable) error
+type Route string
 
 type Routable interface {
-	Route() string
+	Route() Route
 }
 
 type Handler interface {
@@ -18,6 +12,30 @@ type Handler interface {
 
 type HandlerFunc func(Routable) error
 
-type Source interface {
-	Next() (Routable, error)
+func (f HandlerFunc) Process(r Routable) error {
+	return f(r)
+}
+
+type Mux struct {
+	routes map[Route]Handler
+}
+
+func (m *Mux) Handle(route Route, handler Handler) {
+	m.register(route, handler)
+}
+
+func (m *Mux) HandleFunc(route Route, handler func(r Routable) error) {
+	m.register(route, HandlerFunc(handler))
+}
+
+func (m *Mux) register(route Route, handler Handler) {
+	if _, ok := m.routes[route]; !ok {
+		panic("Route already has a registered Handler")
+	}
+	m.routes[route] = handler
+}
+
+func (m *Mux) Process(r Routable) error {
+	h := m.routes[r.Route()]
+	return h.Process(r)
 }
